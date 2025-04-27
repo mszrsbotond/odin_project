341fc104bd0ca748a9f801d2d2b7c352c4026f81
mszrsbotond <botond00723@gmail.com>
2025-04-27 21:33:32 +0200

    finished task 5

--- js_basisc/js_exercises/05_sumAll/sumAll.js
+++ js_basisc/js_exercises/05_sumAll/sumAll.js
@@ -1,5 +1,35 @@
-const sumAll = function() {
+const sumAll = function(from, to)
+{
+    let summed = 0;
+    let bothIntAndPositive = Number.isInteger(from) && Number.isInteger(to) && from >=0 && to >= 0;
+    if(bothIntAndPositive)
+    {
+        if(from > to)
+        {
+            for(let i = to; i <= from; i++)
+                {
+                    summed += i;
+                    console.log(i);
+                }
+            
+                return summed;
+        }
+        else
+        {
+            for(let i = from; i <= to; i++)
+                {
+                    summed += i;
+                    console.log(i);
+                }
+            
+                return summed;
+        }
+    }
 
+    else
+    {
+        return "ERROR"
+    }
 };
 
 // Do not edit below this line
