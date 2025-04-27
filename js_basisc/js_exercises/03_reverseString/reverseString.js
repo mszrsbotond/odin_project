7e45b969f3c278e1973eb2b172fd0eab686aada8
mszrsbotond <botond00723@gmail.com>
2025-04-27 20:20:53 +0200

    finished task 3

--- js_basisc/js_exercises/03_reverseString/reverseString.js
+++ js_basisc/js_exercises/03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string)
+{
+    let reversedStringVariable = "";
+    for(let i = string.length - 1; i > -1; i--)
+    {
+        reversedStringVariable += string[i]
+    }
 
+    return reversedStringVariable;
 };
 
 // Do not edit below this line
