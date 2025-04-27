2908b0216dcef741776ca83b1ee7ea1eb48e57d7
mszrsbotond <botond00723@gmail.com>
2025-04-27 21:08:15 +0200

    finished task 4

--- js_basisc/js_exercises/04_removeFromArray/removeFromArray.js
+++ js_basisc/js_exercises/04_removeFromArray/removeFromArray.js
@@ -1,4 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args)
+{
+    let newArray = [];
+    for(let i in array){
+        let getDeleted = false;
+        for(let j in args){
+            if(array[i] == args[j] && typeof array[i] == typeof args[j]){
+                getDeleted = true;
+            }
+        }
+
+        if(!getDeleted){
+            newArray.push(array[i]);
+        }
+    }
+
+    return newArray;
 };
 
 // Do not edit below this line
