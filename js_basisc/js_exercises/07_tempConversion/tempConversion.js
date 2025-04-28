b45e74fd7c8cfdd064423e3bef3ccacc03e80e70
mszrsbotond <botond00723@gmail.com>
2025-04-28 12:52:18 +0200

    finished task 7

--- js_basisc/js_exercises/07_tempConversion/tempConversion.js
+++ js_basisc/js_exercises/07_tempConversion/tempConversion.js
@@ -1,7 +1,24 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let cels = parseFloat(((temp - 32) / (9/5)).toFixed(1));
+
+  if(cels % 10 == 0){
+    return parseInt(cels);
+  }
+  else{
+    return cels;
+  }
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let fahr = parseFloat((temp * (9/5) + 32).toFixed(1));
+
+  if(fahr % 10 == 0){
+    return parseInt(fahr);
+  }
+  else{
+    return fahr;
+  }
 };
 
 // Do not edit below this line
