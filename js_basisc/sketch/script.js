666c1d5f07ccd935ba3fb6b3cf7549b36ea17a01
mszrsbotond <botond00723gmail.com>
2025-05-15 18:51:05 +0200

    finish sketch

--- js_basisc/sketch/script.js
+++ js_basisc/sketch/script.js
@@ -1,5 +1,26 @@
 const container = document.querySelector(".container")
+const button = document.querySelector("button")
+let newGridSize = -1;
 
+createGrid(16)
+drawing()
+changeGrid()
+
+// create grid function
+function createGrid(rowSize)
+{
+    let squareSize = 800 / rowSize
+    for(let x = 0; x < rowSize * rowSize; x++)
+    {
+        const square = document.createElement("div")
+        square.setAttribute("id", "square")
+        square.style["width"] = squareSize + "px"
+        square.style["height"] = squareSize + "px"
+        container.appendChild(square)
+    }
+}
+
+// random color function
 function getRandomColor()
 {
     let r = Math.floor(Math.random() * 255)
@@ -10,24 +31,36 @@ function getRandomColor()
 }
 
 
-for(let x = 0; x < 256 ; x++)
+function changeGrid()
 {
-    const elem = document.createElement("div")
-    elem.className = `elem elem-${x}`
-    container.appendChild(elem)
+    button.addEventListener("click", () => {
+    console.log(newGridSize)
+    do
+    {
+        newGridSize = prompt("Grid Sizing")
+    } while(newGridSize < 0 || newGridSize > 100)
+    document.querySelectorAll(".container #square").forEach(square => {
+        square.remove() 
+    })
+    createGrid(newGridSize)
+    drawing()
+})
 }
 
-const elems = document.querySelectorAll(".container .elem")
 
-elems.forEach((elem) => {
-    elem.addEventListener("mouseover", (event) => {
-        if(event.buttons == 1)
-        {
-            elem.style.backgroundColor = getRandomColor()
-        }
-        else if(event.buttons == 4)
-        {
-            elem.style.backgroundColor = "white"
-        }
+function drawing()
+{
+    const squares = document.querySelectorAll(".container #square")
+    squares.forEach((square) => {
+        square.addEventListener("mouseover", (event) => {
+            if(event.buttons == 1)
+            {
+                square.style.backgroundColor = getRandomColor()
+            }
+            else if(event.buttons == 4)
+            {
+                square.style.backgroundColor = "white"
+            }
+        })
     })
-})
+}
