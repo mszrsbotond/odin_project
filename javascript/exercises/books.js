b2a1f04a1a3da39b98ac2e5ef1bb60f3a7f22f53
mszrsbotond <botond00723@gmail.com>
2025-05-30 15:11:15 +0200

    finished books task

---
+++ javascript/exercises/books.js
@@ -0,0 +1,18 @@
+function Book(title, author, pages, read){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read
+
+    this.info = function(){
+        if(read == true){
+        console.log(`${title} by ${author}, ${pages} pages, read`)
+        }
+        else{
+        console.log(`${title} by ${author}, ${pages} pages, not read yet`)
+        }
+    }
+}
+
+const harryPotter = new Book("Harry Potter és a Tiltott Varázslat", "J.K. Rowling", 102, false)
+harryPotter.info()
