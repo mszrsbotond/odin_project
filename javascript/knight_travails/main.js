4799613a00dd20b231d5e128a182b47b34bcc396
mszrsbotond <botond00723@gmail.com>
2025-07-10 02:35:24 +0200

    finished project

--- javascript/knight_travails/main.js
+++ javascript/knight_travails/main.js
@@ -1,43 +1,56 @@
-function buildBoard() {
-  let board = [];
-  for (let i = 0; i < 8; i++) {
-    for (let j = 0; j < 8; j++) {
-      board.push([i, j]);
+class knightTravails {
+  constructor() {}
+
+  knightMoves(start, end) {
+    let possibleMoves = [
+      [1, 2],
+      [1, -2],
+      [-1, 2],
+      [-1, -2],
+      [2, 1],
+      [2, -1],
+      [-2, 1],
+      [-2, -1],
+    ];
+
+    // storing the places on the board i already visited, its a Set to make it available for the has check
+    let visited = new Set();
+    let queue = [[start[0], start[1]]];
+    let steps = 0;
+
+    while (queue.length) {
+      let next = [];
+      let current;
+      while (queue.length) {
+        current = queue.shift();
+
+        let currentX = current[0];
+        let currentY = current[1];
+
+        if (currentX === end[0] && currentY === end[1]) {
+          console.log(end)
+          return steps;
+        }
+
+        for (let move of possibleMoves) {
+          let nextX = currentX + move[0];
+          let nextY = currentY + move[1];
+
+          if (!visited.has([nextX + "," + nextY]) && nextX < 8 && nextY < 8 && nextX > -1 && nextY > -1) {
+            visited.add([nextX + "" + nextY]);
+            next.push([nextX, nextY]);
+          }
+        }
+      }
+      steps++;
+      console.log(current)
+      queue = next;
     }
   }
-  return board;
 }
 
-function knightMoves(from, to) {
-  let possibleMoves = [
-    [1, 2],
-    [1, -2],
-    [-1, 2],
-    [-1, -2],
-    [2, 1],
-    [2, -1],
-    [-2, 1],
-    [-2, -1],
-  ];
-}
+let knight = new knightTravails();
+console.log(knight.knightMoves([0,0], [3,3]));
 
-/* function bfs(graph, node){
-    let visited = []
-    let queue = []
 
-    visited.push(node)
-    queue.push(node)
 
-    while(queue.length > 0){
-        s = queue.shift()
-        console.log(s)
-        console.log(visited)
-        console.log(queue)
-        for(n of graph[s]){
-            if(!visited.includes(n)){
-                visited.push(n)
-                queue.push(n)
-            }
-        }
-    }
-} */
