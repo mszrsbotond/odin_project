75e7b050cc100aca95c57f8d9fb6f61580520411
mszrsbotond <botond00723@gmail.com>
2025-07-15 14:41:05 +0200

    working round changing

--- javascript/battleship/src/handleClick.js
+++ javascript/battleship/src/handleClick.js
@@ -1,50 +1,20 @@
-export function handleClickPlayer(player) {
-  const playerBoard = document.querySelectorAll(".playerBoardContainer > div");
-
-  playerBoard.forEach((square) => {
-    square.addEventListener("click", () => {
-      let id = square.id
-      player.gameboard.receiveAttack(id);
-      for (let z = 0; z < 100; z++) {
-        if (
-          id == player.gameboard.board[z].id &&
-          player.gameboard.board[z].ship != null
-        ) {
-          console.log(player.gameboard);
-          square.style["background-color"] = "green";
-        } else if (
-          id == player.gameboard.board[z].id &&
-          player.gameboard.board[z].ship == null
-        ) {
-          square.style["background-color"] = "red";
-        }
-      }
-    });
-  });
+export function handleClick(user, square) {
+  let id = square.id;
+  user.gameboard.receiveAttack(id);
+  for (let z = 0; z < 100; z++) {
+    if (
+      id == user.gameboard.board[z].id &&
+      user.gameboard.board[z].ship != null
+    ) {
+      console.log(user.gameboard);
+      square.style["background-color"] = "green";
+    } else if (
+      id == user.gameboard.board[z].id &&
+      user.gameboard.board[z].ship == null
+    ) {
+      square.style["background-color"] = "red";
+    }
+  }
 }
 
 
-export function handleClickAI(AI) {
-  const AIBoard = document.querySelectorAll(".AIBoardContainer > div");
-
-  AIBoard.forEach((square) => {
-    square.addEventListener("click", () => {
-      let id = square.id
-      AI.gameboard.receiveAttack(id);
-      for (let z = 0; z < 100; z++) {
-        if (
-          id == AI.gameboard.board[z].id &&
-          AI.gameboard.board[z].ship != null
-        ) {
-          console.log(AI.gameboard);
-          square.style["background-color"] = "green";
-        } else if (
-          id == AI.gameboard.board[z].id &&
-          AI.gameboard.board[z].ship == null
-        ) {
-          square.style["background-color"] = "red";
-        }
-      }
-    });
-  });
-}
--- javascript/battleship/src/main.js
+++ javascript/battleship/src/main.js
@@ -1,18 +1,31 @@
-import './style.css'
+import "./style.css";
 
 import { createAIBoard, createPlayerBoard } from "./createBoards";
-import { Gameboard, Player, Ship } from './setup';
-import {handleClickAI, handleClickPlayer } from './handleClick';
+import { Gameboard, Player, Ship } from "./setup";
+import { handleClick } from "./handleClick";
 
-const player = new Player()
-player.gameboard.placeShip(['A0', 'A1', 'A2'])
-player.gameboard.placeShip(['J6', 'J7', 'J8'])
+const player = new Player();
+player.gameboard.placeShip(["A0", "A1", "A2"]);
+player.gameboard.placeShip(["J6", "J7", "J8"]);
 
-const ai = new Player()
-ai.gameboard.placeShip(['A3', 'A4'])
+const ai = new Player();
+ai.gameboard.placeShip(["A3", "A4"]);
 
-createPlayerBoard(10, player)
-createAIBoard(10, ai)
+createPlayerBoard(10, player);
+createAIBoard(10, ai);
 
-handleClickPlayer(player)
-handleClickAI(ai)
+const playerBoard = document.querySelectorAll(".playerBoardContainer > div");
+const aiBoard = document.querySelectorAll(".AIBoardContainer > div");
+
+let currUser = "player";
+playerBoard.forEach((square) => {
+  square.addEventListener("click", () => {
+    if (currUser == "player") {
+      currUser = "ai";
+      handleClick(player, square);
+    }else{
+        alert("It's the AI's turn!")
+        aiTurn()
+    }
+  });
+});
